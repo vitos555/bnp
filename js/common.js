068e0d28641dca7dc43a8b63700ab1d8688f4c32
akella <akella.a@gmail.com>
2015-09-13 19:56:15 +0300

    test

--- js/common.js
+++ js/common.js
@@ -1,19 +1,10 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-	console.log($('body').html());
+	$('.js-togglefooter').click(function(event) {
+		$('.footer__totoggle').slideToggle();
+		$(this).toggleClass('is-active');
+	});
+	$('.js-toggleheader').click(function(event) {
+		$('.create').slideToggle();
+	});
 });
