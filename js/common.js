7203664e3c7bc579a2665119284079a438ce5e61
akella <akella.a@gmail.com>
2015-09-21 18:25:16 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -3,9 +3,11 @@ head.ready(function() {
 	$('.js-togglefooter').click(function(event) {
 		$('.footer__totoggle').slideToggle();
 		$(this).toggleClass('is-active');
+		return false;
 	});
 	$('.js-toggleheader').click(function(event) {
 		$('.create').slideToggle();
+		return false;
 	});
 	$('.item').click(function(event) {
 		$(this).addClass('is-voted');
